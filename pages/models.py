7c47b1385989514ebc53039125319428b8963297
Yousef Alkhalifah <y.alkhalifah@hotmail.com>
2020-01-14 19:28:56 +0300

    God damn detail

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+class Page(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	last_update = models.DateTimeField(auto_now=True)
+
+	def get_absolute_url(self):
+		return "/pages/%i/" %self.id
